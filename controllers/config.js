9b6f211ee109f6e848a609adac82384acda68db8
songyefeng <1352198650@qq.com>
2022-12-30 23:02:25 +0800

    接口重构 大文件上传

---
+++ controllers/config.js
@@ -0,0 +1,9 @@
+exports.ADMIN = 3;
+exports.INPUT = 2;
+exports.APPROVE = 1;
+
+exports.roles = {
+    [exports.ADMIN]: 'administrator',
+    [exports.APPROVE]: 'approve',
+    [exports.INPUT]: 'input'
+}
